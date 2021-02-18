b843289a83d1a3c3faeb2a787b99315a2ad5f47c
Misagh <mm1844@gmail.com>
2021-02-18 19:22:56 +0400

    refactor settings for gradle

--- ci/functions.sh
+++ ci/functions.sh
@@ -6,10 +6,10 @@ function downloadTomcat() {
   tomcatUrl="https://downloads.apache.org/tomcat/tomcat-9/${tomcatVersionTag}/bin/apache-tomcat-${tomcatVersion}.zip"
 
   export CATALINA_HOME=./apache-tomcat-${tomcatVersion}
-  rm -Rf ${CATALINA_HOME}
-  wget --no-check-certificate ${tomcatUrl}
-  unzip apache-tomcat-${tomcatVersion}.zip
-  chmod +x ${CATALINA_HOME}/bin/*.sh
+  rm -Rf ${CATALINA_HOME} > /dev/null 2>&1
+  wget --no-check-certificate ${tomcatUrl} > /dev/null 2>&1
+  unzip apache-tomcat-${tomcatVersion}.zip > /dev/null 2>&1
+  chmod +x ${CATALINA_HOME}/bin/*.sh > /dev/null 2>&1
   rm -Rf ${CATALINA_HOME}/webapps/examples ${CATALINA_HOME}/webapps/docs ${CATALINA_HOME}/webapps/host-manager ${CATALINA_HOME}/webapps/manager
   touch ${CATALINA_HOME}/logs/catalina.out ; tail -F ${CATALINA_HOME}/logs/catalina.out &
 }
